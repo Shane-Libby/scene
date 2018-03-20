c5a88ffe53205062b2019db265c0c0f5dda6202d
Shane & Libby <shane_baker@hotmail.co.uk>
2018-03-20 16:43:21 +0000

    Initial commit

---
+++ Assets/ButtonPush.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonPush : MonoBehaviour {
+
+    public SpriteRenderer sr; //This variable will hold out sprite renderer, so we can access it and change the sprite
+    public Sprite defaultSprite; //This is our default non pushed button sprite
+    public Sprite pressedSprite; //This is our pressed button sprite
+    public AudioSource audioSource; //This is the source of which we will play the button sound through
+    public AudioClip pressSound; //This is the sound that will play when the button is pressed
+    public Test test; //This holds the GameObject of the GameManager so we can send messages more efficiently
+    public int id; //The id is the number allocated to the button ranging from 1 to 4 (green = 1, red = 2, blue = 3, yellow = 4)
+    public bool canClick; //The canClick bool tells us if we can click on the button (true) or if we can't (false)
+
+    void Awake() //The Awake function is called when the script instance is being loaded
+    {
+        sr = transform.GetComponent<SpriteRenderer>(); //Here we are getting our SpriteRenderer component in the transform and assigning it to the variable 'sr'
+        audioSource = GameObject.Find("Camera").GetComponent<AudioSource>();
+        audioSource.volume = PlayerPrefs.GetFloat("Volume"); //We are setting our volume to the one that we set in the options menu 
+    }
+
+    void OnMouseDown() //This function is called whenever you push down on the left mouse button, inside the objects collider
+    {
+        if (canClick)
+        { //The code inside the brackets can only be done if canClick is equal to true you could also write it if(canClick == true)
+            
+            sr.sprite = pressedSprite; //Here we are setting our transform's sprite to the pushed sprite
+            audioSource.PlayOneShot(pressSound); //When we press down on the mouse button the 'pressSound' clip will be played
+            test.StartCoroutine("CheckSequenceButton", id); //When we click the button we want to check if it is the correct one. So we send over our id over to the GameManager to see if it is.
+            transform.localScale -= new Vector3(0.02f, 0.02f, 0.02f); //When we click on the button we reduce the size just a bit to make it loom like the button is being pushed down
+        }
+    }
+
+    void OnMouseUp() //This function is called whenever you let go of the left mouse button, while your cursor is inside the collider
+    {
+        if (canClick)
+        {
+            sr.sprite = defaultSprite; //Here we are setting our transform's sprite back to its original non pushed sprite
+            transform.localScale = new Vector3(0.75f, 0.75f, 0.75f); //When we let go of the button it returns to its original scale
+        }
+    }
+
+    public IEnumerator ButtonPress() //This is the function that GameLogic calls to press the button and play an audio clip
+    {
+        sr.sprite = pressedSprite;
+        audioSource.PlayOneShot(pressSound);
+        transform.localScale -= new Vector3(0.02f, 0.02f, 0.02f);
+        yield return new WaitForSeconds(0.4f); //To make it look like an authentic click, the function waits for 0.4 seconds, then continues with the rest of the code
+        transform.localScale = new Vector3(0.75f, 0.75f, 0.75f);
+        sr.sprite = defaultSprite;
+    }
+
+    public void DisableOrEnableButton(bool i) //The message that GameManager sends, telling the button if it can be clicked or not
+    {
+        canClick = i;
+    }
+}
---
+++ Assets/Test.cs
@@ -0,0 +1,170 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Test : MonoBehaviour
+{
+    public int[] sequence; //The sequence int array will hold our sequence order
+    public int sequenceLength; //The sequenceLength is how long a sequence will be. Everytime the player completes a sequence it goes up one
+    public int sequencesPlayedCom; //The sequencesPlayedCom is to measure how many times the computer has pressed the buttons in the current sequence
+    public int sequencesPlayedPlayer; //The sequencesPlayedPlayer is to measure how many times the player has pressed the buttons in the current sequence
+
+    //Buttons
+    public GameObject greenButton; //This holds the GameObject of our green button, so that the script can send messages over to it
+    public GameObject redButton; //This holds the GameObject of our red button, so that the script can send messages over to it
+    public GameObject blueButton; //This holds the GameObject of our blue button, so that the script can send messages over to it
+    public GameObject yellowButton; //This holds the GameObject of our yellow button, so that the script can send messages over to it
+
+    //Sounds
+    public AudioSource audioSource; //This is the source of which we will play the sequence fail and complete sounds through
+    //public AudioClip sequenceCompleteSound;
+    //public AudioClip sequenceFailSound;
+    public AudioClip buttonClickSound;
+   
+
+
+    public GameObject youWinText;
+    public GameObject youFailedText;
+
+    public string nextLevel = "Level2";
+    public int levelToUnlock = 2;
+
+    public string levelTag;
+    
+    
+
+
+    void Start() //The Start function is called on the frame that the script becomes active
+    {
+        
+        audioSource = GameObject.Find("Camera").GetComponent<AudioSource>();
+        audioSource.volume = PlayerPrefs.GetFloat("Volume"); //We are setting our volume to the one that we set in the options menu
+        sequence = new int[2]; //Here we are defining the sequence array's size
+        StartCoroutine(GenerateNextSequence()); //We call the GenerateNextSequence function which randomly selects the next number for the sequence
+
+
+    }
+    void Update()
+    {
+        
+    }
+
+
+    IEnumerator GenerateNextSequence()
+    {
+            sequencesPlayedCom = 0; //Set the number of times that the computer has clicked a button to 0
+            sequencesPlayedPlayer = 0; //Set the number of times that the player has clicked a button to 0
+
+            {
+                for (int i = 0; i < sequenceLength; i++)
+                {
+                    sequence[i] = Random.Range(1, 5);
+                }
+                yield return new WaitForSeconds(1);
+                StartCoroutine(PlaySequence()); //Now that we have our random sequence, we can have the computer play it
+            }
+    }
+    
+
+    IEnumerator PlaySequence() //Plays the sequence
+    {
+        CanClickButton(false); //We call a function that disables the buttons while the computer is playing the sequence
+
+        while (sequencesPlayedCom < sequenceLength)
+        { //A while loop. Here we have it continuously do what is inside the brackets as long as sequencesPlayedCom < sequenceLength
+            GameObject buttonToPress = null; //We set this temporary GameObject variable to hold the button so its ButtonPress function can be called
+
+            if (sequence[sequencesPlayedCom] == 1)
+            { //If the next element in the int array is 1 then it will be a green button
+                buttonToPress = greenButton;
+            }
+            else if (sequence[sequencesPlayedCom] == 2)
+            {
+                buttonToPress = redButton;
+            }
+            else if (sequence[sequencesPlayedCom] == 3)
+            {
+                buttonToPress = blueButton;
+            }
+            else if (sequence[sequencesPlayedCom] == 4)
+            {
+                buttonToPress = yellowButton;
+            }
+
+            buttonToPress.SendMessage("ButtonPress"); //We call the ButtonPress function on the button so the player can see and hear it
+            yield return new WaitForSeconds(0.7f); //Wait 0.7 seconds until we continue to the next colour in the sequence
+            sequencesPlayedCom++; //Adding one to the sequencesPlayedCom so we know that computer just clicked a button
+        }
+
+        if (sequencesPlayedCom == sequenceLength)
+        {
+            CanClickButton(true); //After the computer has pressed all the buttons in the sequence, the player can now click on the buttons
+        }
+        
+    }
+
+    IEnumerator CheckSequenceButton(int id)
+    {
+        if (sequence[sequencesPlayedPlayer] == id)
+        { //We check to see if the next color of the sequence is the one that the player pressed
+            sequencesPlayedPlayer++;
+
+        }
+        else
+        {
+            youFailedText.SetActive(true);
+            yield return new WaitForSeconds(0.8f); //A delay after the player fails 
+            Application.LoadLevel(3); //Since the player we load up the menu
+
+        }
+
+        if (sequencesPlayedPlayer == sequenceLength)
+        { //If the player has completed the sequence
+            CanClickButton(true);
+            yield return new WaitForSeconds(0.3f); //We have a small delay after the player completes the sequence
+                                                   //audioSource.PlayOneShot(sequenceCompleteSound);
+                                                   // sequenceLength++; //The sequence length is one added to it
+                                                   //StartCoroutine(GenerateNextSequence());//We continue on with this loop, increasing the sequence one colour at a time
+
+            if (sequenceLength == 2) // load the next level here...
+
+            {
+                CanClickButton(false);
+                youWinText.SetActive(true);
+                int currentUnlock = PlayerPrefs.GetInt("levelReached");
+
+                if (currentUnlock < levelToUnlock) 
+
+                PlayerPrefs.SetInt("levelReached", 2);
+                PlayerPrefs.SetInt(levelTag, 1);
+                yield return new WaitForSeconds(0.3f);
+                Application.LoadLevel(0);
+            }
+
+            else
+            {
+                sequenceLength++; //The sequence length is one added to it
+                StartCoroutine(GenerateNextSequence()); //We continue on with this loop, increasing the sequence one colour at a time
+            }
+        }
+    }
+
+
+
+        public void GoToMenu() //Function used to get back to the main menu	
+    {
+        audioSource.PlayOneShot(buttonClickSound);
+        Application.LoadLevel(0);
+    }
+
+
+    void CanClickButton(bool i) //Tells the buttons if they can be clicked or not
+    {
+        greenButton.SendMessage("DisableOrEnableButton", i);
+        redButton.SendMessage("DisableOrEnableButton", i);
+        blueButton.SendMessage("DisableOrEnableButton", i);
+        yellowButton.SendMessage("DisableOrEnableButton", i);
+    }
+}
